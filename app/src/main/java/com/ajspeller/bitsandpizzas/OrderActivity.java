00a2196d430de55fe3cb524416043f71e363e8e3
AJ Speller <ajspeller@gmail.com>
2017-07-21 20:47:56 -0400

    Add Material Design

--- app/src/main/java/com/ajspeller/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/ajspeller/bitsandpizzas/OrderActivity.java
@@ -1,9 +1,12 @@
 package com.ajspeller.bitsandpizzas;
 
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -18,4 +21,19 @@ public class OrderActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
+    public void onClickDone(View view) {
+        //code that runs when the FAB is clicked
+        CharSequence text = "Your order has been updated";
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration);
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(OrderActivity.this, "Undone!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        snackbar.show();
+    }
 }
